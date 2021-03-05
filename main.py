82484b53cc7527010f35a3dfdb0fbfcaef059bb9
theFroggie <47305215+theFroggie@users.noreply.github.com>
2021-03-05 23:53:51 +0000

    Setup basic Example.json file to have an idea of what it would look like + added a simple json loader in main.py

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
 from cogs import app
+import json
+
+with open('RevisionSheet/Example.json', 'r') as myfile:
+    data = myfile.read()
+
+obj = json.loads(data)
+print(obj["Q1name"])
 
 app.Application()
